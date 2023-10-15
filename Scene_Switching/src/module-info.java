7d5072480654dff6b0daed18d95091beea3b197f
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2023-10-15 19:12:24 +0600

    Add files via upload

---
+++ Scene_Switching/src/module-info.java
@@ -0,0 +1,8 @@
+module Scene_Switching {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
